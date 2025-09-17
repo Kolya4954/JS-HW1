466b7b28b286a8ed0ada158cc4c7d9acce4c7e68
Коля <135145397+Kolya4954@users.noreply.github.com>
2025-09-17 16:41:15 +0300

    yay

--- main.js
+++ main.js
@@ -6,3 +6,42 @@ let name = "kolya";
 
 console.log(name);
 
+const isStuden = true;
+
+console.log(isStuden);
+
+const myString = "In the middle of every difficulty lies opportunity.";
+
+console.log(myString);
+
+let myNumber = 13;
+
+myNumber += 10;
+
+console.log(myNumber);
+
+const myNull = null;
+
+console.log(myNull);
+
+const question = prompt("Введіть своє ім’я")
+
+alert(question);
+
+console.log(typeof question);
+
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?")
+
+console.log(userAnswer);
+
+const asking = alert("Увага! Дія може бути небезпечною.")
+
+const checking = confirm("Ви впевнені, що хочете продовжити?")
+
+console.log(checking);
+
+
+
+
+
+
